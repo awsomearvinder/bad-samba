53b581ef55571e0c2f98e86773eea98aed7c4bce
Arvinder <arvinder.dhanoa@winona.edu>
2023-09-11 15:58:06 -0500

    wip: feat(smb-server): Support SMB1 negotiation.

--- smb-server/src/main.rs
+++ smb-server/src/main.rs
@@ -1,11 +1,26 @@
 use std::io;
 
+use smb::Smb1Message;
+use smb2::message::SmbMessage;
+
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let listener = tokio::net::TcpListener::bind("127.0.0.1:445").await?;
     loop {
+        let mut len = [0; 4];
+        let mut buf = Vec::new();
         match listener.accept().await {
-            Ok((socket, addr)) => println!("new client {:?}", addr),
+            Ok((socket, _addr)) => {
+                socket.readable().await.unwrap();
+                socket.try_read(&mut len).expect("failed to read buffer");
+                let len = u32::from_be_bytes(len).try_into().unwrap();
+                buf.resize(len, 0);
+                socket.try_read(&mut buf).unwrap();
+                let negotiate = SmbMessage::try_parse(&buf);
+                println!("SMB2: {:?}", negotiate);
+                let negotiate = Smb1Message::try_parse(&buf);
+                println!("SMB: {:?}", negotiate);
+            }
             Err(e) => println!("Couldn't get client {:?}", e),
         }
     }
